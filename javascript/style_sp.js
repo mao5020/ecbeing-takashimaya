3602cd9a8cb9a19b61d3f5d18cc8ad15030fbf52
hayashi-ern <ern.hayashi60@gmail.com>
2023-08-17 17:30:01 +0900

    特集タブ画像内包全ページ適応、SP用モーダルデザイン制作

--- javascript/style_sp.js
+++ javascript/style_sp.js
@@ -12,3 +12,19 @@ $(document).ready(function(){
 			btnMyList.add(btnShare).wrapAll('<div class="c-modal-item-btn-under"></div>');
 	});
 });
+
+
+$(document).ready(function(){
+	$(".c-itemdetail-list").each(function(){
+			var title = $(this).find(".c-itemdetail-title");
+						title.insertBefore($(this).find(".modal-slider-item-imgwrap"));
+	});
+});
+
+$(document).ready(function(){
+	$(".c-itemdetail-list").each(function(){
+			var btnMyList = $(this).find(".c-modal-item-btn-mylist");
+			var btnShare = $(this).find(".c-modal-item-btn-share");
+			btnMyList.add(btnShare).wrapAll('<div class="c-modal-item-btn-under"></div>');
+	});
+});
